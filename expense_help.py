7f1f55ccd86c15d767e63e78470b56628d67a05c
Christian Dähn <christian.daehn@it-agile.de>
2012-02-23 15:49:33 +0100

    fetching mails from different inboxes

--- expense_help.py
+++ expense_help.py
@@ -3,31 +3,38 @@ Created on Feb 23, 2012
 
 @author: cda
 '''
-import imaplib
 import getpass
+import logging
+import logging.config
+import sys
+from mail.imap import connector_for
+from email.header import decode_header
+
+def remove_encoding(text):
+    texts_with_charsets = decode_header(text)
+    decoded_text = ''
+    for twc in texts_with_charsets:
+        decoded_text += twc[0]
+    return decoded_text
+
 
 def main():
-    print('connect to server...')
-    imap = imaplib.IMAP4_SSL('imap.googlemail.com', 993)
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+    connection = connector_for('imap.googlemail.com')
     
     username = 'christian.daehn@it-agile.de'
     password = getpass.getpass('password for [%s]: ' % username)
-    imap.login(username, password)
+    connection._login(username, password)
     
-    print('fetching expense inboxes...')
-    status, available_inboxes = imap.list()
-    assert status == 'OK', status
-    expense_inboxes = filter(lambda inbox: inbox.startswith('(\\HasNoChildren) "/" "spesen/'), available_inboxes)
+    expense_inboxes = connection.filter_inboxes(lambda inbox: inbox.startswith('"spesen/'))
     print(expense_inboxes)
-    imap.select('spesen')
-    response, serach_data = imap.search(None, 'ALL')
-    ids = serach_data[0].split()
-
-    response, raw_uids = imap.uid('FETCH', '1:*', '(UID)')
     
-    print(raw_uids)
+    for inbox in expense_inboxes:
+        emails = connection.read_from(inbox)
+        print(map(lambda x: remove_encoding(x['Subject']), emails))
 
-    imap.close()
+    connection.close()
 
 
 if __name__ == '__main__':
---
+++ mail/imap.py
@@ -0,0 +1,61 @@
+'''
+Created on Feb 23, 2012
+
+@author: cda
+'''
+import logging
+import email
+import imaplib
+
+class ImapConnector(object):
+    def __init__(self, imap):
+        self.imap = imap
+        self.log = logging.getLogger('ImapConnector')
+        
+    def _login(self, username, password):
+        self.log.info('logging in [%s]' % username)
+        self.imap.login(username, password)
+        
+    def filter_inboxes(self, criteria):
+        status, available_inboxes = self.imap.list()
+        assert status == 'OK', status
+        filtered_inboxes = filter(
+                                    criteria, 
+                                    map(lambda inbox: inbox.replace('(\\HasNoChildren) "/" ', ''), available_inboxes)
+                                    )
+        self.log.debug('filtered %s from available inboxes: %s' % (filtered_inboxes, available_inboxes))
+        return filtered_inboxes
+    
+    def __search_uids(self):
+        response, uids_data = self.imap.uid('search', None, 'ALL')
+        assert response == 'OK', response
+        uids = uids_data[0].split()
+        return uids
+
+    def __fetch_emails(self, uids):
+        emails = []
+        for uid in uids:
+            response, rfc822_mail = self.imap.uid('fetch', uid, '(RFC822)')
+            assert response == 'OK', response
+            emails.append(email.message_from_string(rfc822_mail[0][1]))
+            
+        return emails
+    
+    def read_from(self, inbox):
+        self.imap.select(inbox, readonly = True)
+
+        uids = self.__search_uids()
+        
+        self.log.info('reading [%d] mails from [%s]' % (len(uids), inbox))        
+        self.log.debug('processing %s' % (uids))
+        
+        emails = self.__fetch_emails(uids)        
+        return emails
+    
+    def close(self):
+        self.imap.close()
+
+def connector_for(server):
+    log = logging.getLogger('ImapConnectorFactory')
+    log.info('connecting to server [%s]...' % server)
+    return ImapConnector(imaplib.IMAP4_SSL(server, 993))
