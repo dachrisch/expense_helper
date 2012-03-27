58fb837eb7b202dd53c67327b0ec9ee41ca9b834
Christian Dähn <christian.daehn@it-agile.de>
2012-03-27 11:50:51 +0200

    Revert "unencode header in confirmation"
    This reverts commit 28c5308fe9581442f5234b1afe08f321b169a084.

--- handler/provider.py
+++ handler/provider.py
@@ -7,7 +7,6 @@ Created on Feb 28, 2012
 '''
 import logging
 import getpass
-from mail.imap import unencode_header
 class CommandlinePasswordProvier(object):
     @staticmethod
     def password(username):
@@ -20,7 +19,7 @@ class ConfirmationProvier(object):
         accepting = ('j', 'y')
         if len(emails):
             log.warn('about to forward [%d] emails to [%s]:' % (len(emails), emails[0]['To']))
-            log.info('\n'.join(map(lambda mail: unencode_header(mail['Subject']), emails)))
+            log.info('\n'.join(map(lambda x: x['Subject'], emails)))
             return raw_input("%s to continue or any other key to abort: " % str(accepting)).lower() in accepting
         else:
             log.warn('all mails rejected.')
